957f5e2b2525dcd8d59e897a19cbdec79acfd3af
kreny21 <roma.krenitsyn@gmail.com>
2025-08-08 21:05:59 +1000

    v1

---
+++ handler.php
@@ -0,0 +1,41 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $client_id = filter_var($_GET['client_id'],FILTER_VALIDATE_INT);
+    if($client_id){
+        require_once 'config.php';
+        require_once 'utils.php';
+        try{
+            if(DBUtils::is_user_exist($db,$client_id)){
+                if (DBUtils::is_user_reviewed($db,$client_id)) {
+                    http_response_code(400);
+                    die('Вы уже оставляли отзыв ранее');
+                }
+                $grade = $_POST['grade'];
+                $comment = $_POST['comment'] ?? null;
+                if(in_array($grade,['1','2','3','4','5'])){
+                    $insert_query = $db->prepare('insert into reviews(client_id, grade, comment) values (?, ?, ?)');
+                    $insert_query->execute([$client_id, $grade, $comment]);
+                    http_response_code(200);
+                    die('Успех!');
+                }
+            }
+            else{
+                http_response_code(400);
+                die('Пользователь не существует');
+            }
+    }
+    catch(PDOException $e){
+        error_log('Ошибка при работе с БД'. $e->getMessage());
+        die;
+    }
+    }
+    else{
+        http_response_code(400);
+        die('Некорректный id клиента');
+    }
+}
+else{
+    http_response_code(405);
+    die('Метод не разрешен');
+}
+?>
---
+++ utils.php
@@ -0,0 +1,14 @@
+<?php
+class DBUtils{
+    public static function is_user_exist(PDO $db, $client_id):bool {
+        $check_user_query = $db->prepare("select client_id from clients where client_id = ?");
+        $check_user_query->execute([$client_id]);
+        return (bool) $check_user_query->fetch();
+    }
+    public static function is_user_reviewed(PDO $db, $client_id):bool {
+        $check_query = $db->prepare('select client_id from reviews where client_id = ? limit 1');
+        $check_query->execute([$client_id]);
+        return (bool) $check_query->fetch();
+    }
+}
+?>
